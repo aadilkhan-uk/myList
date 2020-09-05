35ebd581efeb00a45b6986ac818ea079fe538898
Aadil Khan <aadil.khan6425@gmail.com>
2020-09-05 19:42:11 +0100

    Updates

---
+++ precache-manifest.b63ffbf44423a68f9907bbb78b451319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4112ee8be152886b11d5668d294f8562",
+    "url": "/mylist/index.html"
+  },
+  {
+    "revision": "f45acaed01529a696ceb",
+    "url": "/mylist/static/css/main.bc8c8758.chunk.css"
+  },
+  {
+    "revision": "b4836e55160618524243",
+    "url": "/mylist/static/js/2.7b31d815.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mylist/static/js/2.7b31d815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f45acaed01529a696ceb",
+    "url": "/mylist/static/js/main.11239daa.chunk.js"
+  },
+  {
+    "revision": "1c09ec7f460a2f7ed099",
+    "url": "/mylist/static/js/runtime-main.34b0d3bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mylist/static/media/logo.5d5d9eef.svg"
+  }
+]);
