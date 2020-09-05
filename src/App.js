f08173a983812b66a845043425052dbe752ab1b9
Aadil Khan <aadil.khan6425@gmail.com>
2020-09-05 22:06:15 +0100

    changing to hashrouter

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Aux from './hoc/Aux/Aux';
 import Layout from './Components/Layout/Layout';
 import styles from './App.module.css';
 import Welcome from './Components/Welcome/Welcome';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename="/mylist">
+    <HashRouter basename="/mylist">
       <div className={styles.Main}>
         <Welcome />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
