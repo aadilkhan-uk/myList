2e4b1f9ef4997e824084dd397b3267c87c83aea1
Aadil Khan <aadil.khan6425@gmail.com>
2020-09-05 22:00:53 +0100

    changing browserroute 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename="/my-list">
+    <BrowserRouter basename="/mylist">
       <div className={styles.Main}>
         <Welcome />
       </div>
